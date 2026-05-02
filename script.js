66ae3c296c4ed9ba8ec97fabded798b9a4a5d919
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-02 23:18:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+// day 1
+function memoize(fn) {
+    let cache = new Map();
+
+    return function (...args) {
+        let key = args.toString();
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        let result = fn(...args);
+        cache.set(key, result);
+
+        return result;
+    };
+}
+
+
+// day 2
+function createCounter(n) {
+    return function() {
+        return n++;   
+    };
+}
+
+//day  3
+function expect(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function(val2) {
+            if (val !== val2) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+}
 
