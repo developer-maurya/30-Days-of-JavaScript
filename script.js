0a6cf5c4c06a334df4fa5d3d52ec2f4052607bbd
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-04 23:06:28 +0530

    Refactor memoize and createCounter functions

--- script.js
+++ script.js
@@ -66,4 +66,13 @@ function createCounter(init) {
     };
 }
 
+// day5
+function map(arr, fn) {
+    let result = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = fn(arr[i], i);  
+    }
+
+    return result;
+}
