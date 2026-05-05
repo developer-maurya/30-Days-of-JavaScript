311f65275898cc7c3a6f90d62130c6f08ef6f324
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-05 23:42:30 +0530

    Add filter function and refactor existing functions

--- script.js
+++ script.js
@@ -77,3 +77,15 @@ function map(arr, fn) {
     return result;
 }
 
+//day 6
+function filter(arr, fn) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {   
+            result.push(arr[i]); 
+        }
+    }
+
+    return result;
+}
