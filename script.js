ff36f5a8844b43ec75746aefab770d99465691f6
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-07 22:55:07 +0530

    Refactor and add new functions in script.js

--- script.js
+++ script.js
@@ -101,3 +101,9 @@ function reduce(nums, fn, init) {
 }
 
 // day 8
+var addTwoPromises = async function(promise1, promise2) {
+    let value1 = await promise1;
+    let value2 = await promise2;
+
+    return value1 + value2;
+};
