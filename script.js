b7930e6be3ca56eb221ccab93e3afbf15951efe7
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-08 23:40:19 +0530

    Refactor and add compose function to script.js

--- script.js
+++ script.js
@@ -108,3 +108,11 @@ var addTwoPromises = async function(promise1, promise2) {
     return value1 + value2;
 };
 // day 9
+var compose = function(functions) {
+    return function(x) {
+        for (let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
+        return x;
+    };
+};
