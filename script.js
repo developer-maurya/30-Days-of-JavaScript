1bbd19a29a2e9c7dfefff2ef0c31ef242a109f67
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-14 22:51:23 +0530

    Add timeLimit function to enforce execution time

--- script.js
+++ script.js
@@ -154,3 +154,24 @@ async function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
 }
 // day 14
+
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timer);
+                    resolve(result);
+                })
+                .catch((err) => {
+                    clearTimeout(timer);
+                    reject(err);
+                });
+        });
+    }
+};
