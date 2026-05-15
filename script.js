38b005f18491828ed1bfaec364e7e7926d73813f
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-15 23:09:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -177,3 +177,39 @@ var timeLimit = function(fn, t) {
 };
 
 // day 15
+class TimeLimitedCache {
+    constructor() {
+        this.cache = new Map();
+    }
+
+    set(key, value, duration) {
+        const found = this.cache.has(key);
+
+        if (found) {
+            clearTimeout(this.cache.get(key).timeout);
+        }
+
+        const timeout = setTimeout(() => {
+            this.cache.delete(key);
+        }, duration);
+
+        this.cache.set(key, {
+            value,
+            timeout
+        });
+
+        return found;
+    }
+
+    get(key) {
+        if (this.cache.has(key)) {
+            return this.cache.get(key).value;
+        }
+
+        return -1;
+    }
+
+    count() {
+        return this.cache.size;
+    }
+}
