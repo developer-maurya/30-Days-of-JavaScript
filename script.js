a94c0ece5075acf261b904e6051c2ab3325cd6cd
Chitranjan kumar <chitranjan75075@gmail.com>
2026-05-18 23:16:21 +0530

    Restore and add new functions in script.js

--- script.js
+++ script.js
@@ -242,3 +242,24 @@ var debounce = function(fn, t) {
 };
  
 // day 18
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        let completed = 0;
+
+        functions.forEach((fn, index) => {
+            fn()
+                .then((value) => {
+                    results[index] = value;
+                    completed++;
+
+                    if (completed === functions.length) {
+                        resolve(results);
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    });
+};
